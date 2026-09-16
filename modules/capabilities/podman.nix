5270b2f8c7a2c1a55d48501d9689317d3f38f392
Meg Richards <mouse@cmu.edu>
2026-09-16 09:21:32 -0400

    fix: explicitly allocate deploy podman IDs

--- modules/capabilities/podman.nix
+++ modules/capabilities/podman.nix
@@ -10,6 +10,7 @@
   # /home/deploy/.config/containers/systemd, and persistent app data below
   # /srv should be owned by deploy.
   users.users.deploy.linger = true;
+  users.users.deploy.autoSubUidGidRange = true;
 
   # Quadlet units are declarative files in deploy's user systemd manager.
   # The NixOS module installs Podman's user generator; Home Manager owns the
@@ -24,7 +25,6 @@
     };
   };
   #
-
 
 
 }
