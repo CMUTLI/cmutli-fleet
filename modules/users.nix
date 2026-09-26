2cc3cb51e649ad4c7dd8d0c60d6c1c4870dd6af6
Meg Richards <mouse@cmu.edu>
2026-09-25 20:49:37 -0400

    fix: give deploy its own primary group

--- modules/users.nix
+++ modules/users.nix
@@ -10,9 +10,13 @@
   # Andrew Kerberos through SSH keyboard-interactive authentication.
   users.mutableUsers = false;
 
+  # deploy has its own primary group so service data below /srv can be
+  # restricted to it rather than shared with every normal user.
+  users.groups.deploy.gid = 1000;
   users.users.deploy = {
     isNormalUser = true;
     uid = 1000;
+    group = "deploy";
     hashedPassword = "!";
     openssh.authorizedKeys.keys = [ ];
   };
